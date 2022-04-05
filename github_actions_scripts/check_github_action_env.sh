f862f15a7a50f6cfd9b75f07c076f70d4aafc7ae
hp-cv <102991749+hp-cv@users.noreply.github.com>
2022-04-04 17:24:11 -0700

    yes

---
+++ github_actions_scripts/check_github_action_env.sh
@@ -0,0 +1,20 @@
+            echo hello, world
+            cat /proc/cpuinfo
+            pwd
+            ls -a
+            echo show current versions
+            git --version
+            python --version
+            pip --version
+            pip list
+            #apt search cuda
+            echo workspace location
+            echo $RUNNER_WORKSPACE
+            echo who is running this script
+            whoami
+            echo how is the disc laid out
+            df -h
+            echo avaialable env variables
+            env
+            
+            apt install -y curl
